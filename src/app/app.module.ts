cf893fba4640e81ec86ad4083a68fa5220fe41e2
vinodkumarcvk <54588850+vinodkumarcvk@users.noreply.github.com>
2022-11-07 13:31:16 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,26 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+
+  declarations: [ 
+    AppComponent, 
+    HelloComponent ,
+    ParentComponent,
+    ChildComponent
+  ],
+
+  imports:[
+     BrowserModule,
+     FormsModule,
+    AppRoutingModule 
+  ],
+
+
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
